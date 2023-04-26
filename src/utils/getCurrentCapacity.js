43d99dd36f17ddebf2098ffeb4a4ad09f3078318
Carlos Mario <cmescandonpardo@gmail.com>
2023-04-26 11:54:16 -0500

    Feature: Util - getCurrentCapacity

---
+++ src/utils/getCurrentCapacity.js
@@ -0,0 +1,20 @@
+const awgCurrent = {
+    100: 2,
+    80: 4,
+    60: 6,
+    40: 8,
+    30: 10,
+    20: 12,
+    10: 14
+};
+
+function getCurrentCapacity(current) {
+    for (let currentVal in awgCurrent) {
+        if (currentVal >= current) {
+            return awgCurrent[currentVal];
+        }
+    }
+    return null; // Retorna null si no se encuentra ningún calibre correspondiente
+}
+
+console.log(findAwg(22));
