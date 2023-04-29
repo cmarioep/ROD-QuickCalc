a404a0c2398281036eaacbd087b81b2c92d2ba56
cmarioep <cmescandonpardo@gmail.com>
2023-04-29 11:53:54 -0500

    Styles: basic styles on children layout pages

--- src/pages/CurrentCapacity.jsx
+++ src/pages/CurrentCapacity.jsx
@@ -1,9 +1,11 @@
+import '../styles/pages/CurrentCapacity.scss';
+
 export const CurrentCapacity= () => {
 
     return (
-        <>
+        <div className="current-capacity">
             <h3>Capacidad de Corriente</h3>
-        </>
+        </div>
     )
 
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 
+import '../styles/pages/Home.scss';
+
 export const Home = () => {
 
     return (
-        <>
+        <div className="home">
             <Link to="/voltagedrop">Caida de Tension</Link>
             <Link to="/currentcapacity">Capacidad de Corriente</Link>
-        </>
+        </div>
     )
 
 }
