5623117e6e7e8fda610fdad6e479f787ea92c95f
cmarioep <cmescandonpardo@gmail.com>
2023-04-30 18:53:05 -0500

    Feature: vite-plugin-pwa basi config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from "vite-plugin-pwa";
+
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA(
+      {
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'My Awesome App',
+          short_name: 'MyApp',
+          description: 'My Awesome App description',
+          theme_color: '#54893e',
+          icons: [
+            {
+              src: 'src/assets/Isotipo--300.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'src/assets/Isotipo--300.png',
+              sizes: '512x512',
+              type: 'image/png'
+            },
+            {
+              src: 'src/assets/Isotipo--300.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable'
+            },
+
+          ],
+          handler: "CacheFirst",
+          devOptions: {
+            enabled: true
+          }
+        }
+      }
+    )
+  ],
 })
