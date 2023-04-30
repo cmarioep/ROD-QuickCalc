c2909b695b8183c992832958323921e50cbc3233
cmarioep <cmescandonpardo@gmail.com>
2023-04-30 12:11:18 -0500

    Feature: semantic structor for home items and basic styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,11 @@ import '../styles/pages/Home.scss';
 export const Home = () => {
 
     return (
-        <div className="home">
-            <Link to="/voltagedrop">Caida de Tension</Link>
-            <Link to="/currentcapacity">Capacidad de Corriente</Link>
-        </div>
+        <ul className="home">
+            <li><Link className="home__item" to="/voltagedrop">Caida de Tensión</Link></li>
+            <li><Link className="home__item" to="/currentcapacity">Selección de Alimentador</Link></li>
+            <li><Link className="home__item" to="/currentcapacity">Capacidad de Corriente</Link></li>
+        </ul>
     )
 
 }
