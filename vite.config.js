f22f932980c8b4e289e8fe624561a463dc6a35f0
cmarioep <cmescandonpardo@gmail.com>
2023-05-01 08:55:10 -0500

    Feature: pwa assets icons

--- vite.config.js
+++ vite.config.js
@@ -19,17 +19,17 @@ export default defineConfig({
           theme_color: '#54893e',
           icons: [
             {
-              src: 'src/assets/Isotipo--300.png',
+              src: 'src/assets/pwa/Isotipo-192x192.png',
               sizes: '192x192',
               type: 'image/png'
             },
             {
-              src: 'src/assets/Isotipo--300.png',
+              src: 'src/assets/pwa/Isotipo-512x512.png',
               sizes: '512x512',
               type: 'image/png'
             },
             {
-              src: 'src/assets/Isotipo--300.png',
+              src: 'src/assets/pwa/Isotipo-512x512.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any maskable'
