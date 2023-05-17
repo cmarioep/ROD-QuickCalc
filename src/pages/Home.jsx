1ba1d24c10951e3dfbba686aff3e5a910a7ce36e
cmarioep <cmescandonpardo@gmail.com>
2023-05-17 09:59:04 -0500

    Feature: img on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,40 @@
 import { Link } from "react-router-dom";
 import { ForwardIcon } from "../components/icons/ForwardIcon";
+
 import '../styles/pages/Home.scss';
 
 
 export const Home = () => {
 
     return (
-        <ul className="home">
-            <li>
-                <Link className="home__item" to="/voltagedrop">
-                    Caida de Tensión
-                    <ForwardIcon />
-                </Link>
-            </li>
-            <li>
-                <Link className="home__item" to="/awgbycurrent">
-                    Selección de Alimentador
-                    <ForwardIcon />
-                </Link>
+        <div className="home">
 
-            </li>
-            <li>
-                <Link className="home__item" to="/currentcapacity">
-                    Llenado de Bandeja
-                    <ForwardIcon />
-                </Link>
-            </li>
-        </ul>
+            <figure>
+                <img src="src/assets/home-splash.webp" alt="Imagen del Home" />
+            </figure>
+
+            <ul className="home__items">
+                <li>
+                    <Link className="home__items__item" to="/voltagedrop">
+                        Caida de Tensión
+                        <ForwardIcon />
+                    </Link>
+                </li>
+                <li>
+                    <Link className="home__items__item" to="/awgbycurrent">
+                        Selección de Alimentador
+                        <ForwardIcon />
+                    </Link>
+
+                </li>
+                <li>
+                    <Link className="home__items__item" to="/currentcapacity">
+                        Llenado de Bandeja
+                        <ForwardIcon />
+                    </Link>
+                </li>
+            </ul>
+        </div>
     )
 
 }
