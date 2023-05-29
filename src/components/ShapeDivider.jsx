3bf8ddccb13c0626b34d2452786cb82b87dc3a28
cmarioep <cmescandonpardo@gmail.com>
2023-05-28 23:09:19 -0500

    Fix: focus on VoltageDrop inputs

--- src/components/ShapeDivider.jsx
+++ src/components/ShapeDivider.jsx
@@ -6,15 +6,15 @@ export const ShapeDivider = () => {
 
         <>
 
-            <div class="custom-shape-divider-bottom-1684696127">
+            <div className="custom-shape-divider-bottom-1684696127">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+                    <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill"></path>
                 </svg>
             </div>
 
-            <div class="custom-shape-divider-bottom-1684696687">
+            <div className="custom-shape-divider-bottom-1684696687">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+                    <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill"></path>
                 </svg>
             </div>
 
